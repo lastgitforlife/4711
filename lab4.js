9c8c94b01a9b826b8740ba201c65d936d7853a53
Garrett Tailby <lastgameforlife@hotmail.com>
2020-02-09 15:26:16 -0800

    Finished Lab 4. Added search and local storage.

--- lab4.js
+++ lab4.js
@@ -1,5 +1,11 @@
+const myStorage = window.localStorage;
 let storageCount = 0;
 
+try{ // see if there is artists int storage.
+    storageCount = Number(myStorage.getItem("storageCount"));
+}catch{
+    console.log("No retrieved artists from storage.");
+}
 
 function addArtist(){
     try{
@@ -35,12 +41,22 @@ function addArtist(){
     }
 }
 
-function newArtist() {
+function newArtist(name = document.getElementById("AAF-name").value,
+                   desc = document.getElementById("AAF-desc").value,
+                   img = document.getElementById("AAF-img").value,
+                   id = 0,
+                   newArtist = true) {
     try{
-        let name = document.getElementById("AAF-name").value;
-        let desc = document.getElementById("AAF-desc").value;
-        let img = document.getElementById("AAF-img").value;
-        addArtist();
+        if(newArtist){
+            addArtist();
+            storageCount++;
+            myStorage.setItem("storageCount", storageCount.toString());
+            myStorage.setItem("ArtistName"+storageCount, name); // Can change this to random number later.
+            myStorage.setItem("ArtistDesc"+storageCount, desc); // Same here
+            myStorage.setItem("ArtistImg"+storageCount, img); // And Here
+            myStorage.setItem("ArtistId"+storageCount, storageCount.toString());
+            id = storageCount;
+        }
         let artistDiv = document.createElement("DIV");
         artistDiv.setAttribute("class", "Artist");
         let artistInfo = document.createElement("DIV");
@@ -53,23 +69,81 @@ function newArtist() {
         artistDesc.textContent = desc;
         let deleteBtn= document.createElement("BUTTON");
         deleteBtn.textContent = "Delete";
-        deleteBtn.addEventListener("click", f => {artistDiv.remove(); storageCount--;});
+        deleteBtn.addEventListener("click", f => {persistentDataRemover(artistDiv, id)}); // persitent Data Remover also removes the item visually.
         artistDiv.appendChild(artistImg);
         artistDiv.appendChild(artistInfo);
         artistInfo.appendChild(artistName);
         artistInfo.appendChild(artistDesc);
         artistInfo.appendChild(deleteBtn);
         document.getElementById("DirectoryOutput").appendChild(artistDiv);
-        storageCount++;
     }catch(e){
         console.log("Error" + e);
     }
 }
 
-function deleteParent(id){
-    let idb = document.getElementById(id);
-    console.log(idb);
+function persistentDataRemover(item, id){
+
+    myStorage.removeItem("ArtistName"+id);
+    myStorage.removeItem("ArtistDesc"+id);
+    myStorage.removeItem("ArtistImg"+id);
+    myStorage.removeItem("ArtistId"+id);
+    item.remove();
 }
 
-let myStorage = window.localStorage;
+function persistentDataReAdder(){
+    for(let i = storageCount; i > 0; i--){
+        try{
+            let name = myStorage.getItem("ArtistName"+i);
+            if(name === null) throw "isEmpty";
+            let desc = myStorage.getItem("ArtistDesc"+i);
+            let img = myStorage.getItem("ArtistImg"+i);
+            let id = myStorage.getItem("ArtistId"+i);
+            newArtist(name, desc, img, id, false);
+        }catch(e) {
+            console.log(e);
+        }
+    }
+}
 
+function search(){ // Uses search bar to reveal relevant artists.
+    let who = document.getElementById("GET-artist").value;
+    console.log(who);
+    let directory = document.querySelector("#DirectoryOutput");
+    let child = directory.lastElementChild;
+    while(child){
+        directory.removeChild(child);
+        child = directory.lastElementChild;
+    }
+    if(who === ""){
+        persistentDataReAdder();
+    }else{
+        for(let i = storageCount; i > 0; i--){
+            let name = myStorage.getItem("ArtistName"+i);
+            if(isIn(who, name)){
+                let desc = myStorage.getItem("ArtistDesc"+i);
+                let img = myStorage.getItem("ArtistImg"+i);
+                let id = myStorage.getItem("ArtistId"+i);
+                newArtist(name, desc, img, id, false);
+            }
+        }
+    }
+}
+
+function isIn(str1, str2){ // Checks to see if str1 is in str2.
+    let charStr1 = str1.split("");
+    let charStr2 = str2.split("");
+    if(charStr1.length > charStr2.length) return(false);
+    let lettersCorrect = 0; // How many letters from str1 found so far in a row in str2.
+    for(let i = 0; i < charStr2.length; i++){
+        if(charStr1[lettersCorrect] === charStr2[i]) lettersCorrect++;
+        else lettersCorrect = 0;
+        if(lettersCorrect === charStr1.length) return(true);
+    }
+    return(false);
+}
+
+document.onreadystatechange = () => {
+    if(document.readyState === "complete"){
+        persistentDataReAdder();
+    }
+};
